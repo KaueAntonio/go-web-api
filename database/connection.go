f6f78a286b08923544805c49594eca4b0f601489
KaueAntonio <kaue4991@gmail.com>
2023-09-19 14:06:06 -0300

    Feat: Configurations

---
+++ database/connection.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	config "web/app/api/Config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Database struct {
+	SqlDb *sql.DB
+}
+
+var dbContext = context.Background()
+
+func OpenConnection() (*sql.DB, error) {
+	cfg := config.GetDbConnection()
+
+	conn, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
