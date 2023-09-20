ef2022c856f3023ba9b9f278c09528c2c502471b
KaueAntonio <kaue4991@gmail.com>
2023-09-20 19:21:09 -0300

    Refactor: Modifying Error Handler

--- api/middlewares/error/error.go
+++ api/middlewares/error/error.go
@@ -3,35 +3,26 @@ package erroService
 import (
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/go-chi/chi"
 )
 
-func Error404(w http.ResponseWriter, r *http.Request) {
+func Error(w http.ResponseWriter, r *http.Request) {
 
 	msg := chi.URLParam(r, "msg")
 
-	w.WriteHeader(http.StatusNotFound)
+	codeParam := chi.URLParam(r, "code")
 
-	http.Error(w, decodeMsg(msg, w), 404)
-}
+	code, err := strconv.ParseInt(codeParam, 0, 64)
 
-func Error500(w http.ResponseWriter, r *http.Request) {
+	if err != nil {
+		panic("Erro ao decodificar código de erro!")
+	}
 
-	msg := chi.URLParam(r, "msg")
+	w.WriteHeader(int(code))
 
-	w.WriteHeader(http.StatusInternalServerError)
-
-	http.Error(w, decodeMsg(msg, w), 500)
-}
-
-func Error401(w http.ResponseWriter, r *http.Request) {
-
-	msg := chi.URLParam(r, "msg")
-
-	w.WriteHeader(http.StatusUnauthorized)
-
-	http.Error(w, decodeMsg(msg, w), 401)
+	http.Error(w, decodeMsg(msg, w), int(code))
 }
 
 func decodeMsg(msg string, w http.ResponseWriter) (m string) {
